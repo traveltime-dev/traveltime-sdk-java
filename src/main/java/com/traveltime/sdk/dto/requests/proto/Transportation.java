60c2b4735141c27e96e255a216926af0037c741d
danielnaumau <sadfreddy@gmail.com>
2022-09-07 11:37:14 +0200

    add automatic version update

--- src/main/java/com/traveltime/sdk/dto/requests/proto/Transportation.java
+++ src/main/java/com/traveltime/sdk/dto/requests/proto/Transportation.java
@@ -9,7 +9,10 @@ public enum Transportation {
     PUBLIC_TRANSPORT("pt", 0),
     WALKING_FERRY("walking+ferry", 7),
     CYCLING_FERRY("cycling+ferry", 6),
-    DRIVING_FERRY("driving+ferry", 3);
+    DRIVING_FERRY("driving+ferry", 3),
+    WALKING("walking", 4),
+    CYCLING("cycling", 5),
+    DRIVING("driving", 1);
 
     private final String value;
     private final Integer code;
