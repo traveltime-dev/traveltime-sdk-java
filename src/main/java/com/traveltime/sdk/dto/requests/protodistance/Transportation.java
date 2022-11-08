5ec160c97ac2cd4d3f76e55a934f4adc6079e140
Marcin Januszkiewicz <marcin.januszkiewicz@traveltime.com>
2022-11-08 11:43:56 +0100

    Add modes without ferry to distance requests.

--- src/main/java/com/traveltime/sdk/dto/requests/protodistance/Transportation.java
+++ src/main/java/com/traveltime/sdk/dto/requests/protodistance/Transportation.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Transportation {
+    DRIVING("driving", 1),
     DRIVING_FERRY("driving+ferry", 3),
+    WALKING("walking", 4),
     WALKING_FERRY("walking+ferry", 7);
 
     private final String value;
