26acc0979d6a331c3dcb0c1dab6296c4e85b26cc
Marcin Januszkiewicz <marcin.januszkiewicz@traveltime.com>
2022-11-14 16:12:37 +0100

    Make transportation an interface to allow custom countries to be used if needed.

--- src/main/java/com/traveltime/sdk/dto/requests/proto/Transportation.java
+++ src/main/java/com/traveltime/sdk/dto/requests/proto/Transportation.java
@@ -3,17 +3,37 @@ package com.traveltime.sdk.dto.requests.proto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-public enum Transportation {
-    PUBLIC_TRANSPORT("pt", 0),
-    WALKING_FERRY("walking+ferry", 7),
-    CYCLING_FERRY("cycling+ferry", 6),
-    DRIVING_FERRY("driving+ferry", 3),
-    WALKING("walking", 4),
-    CYCLING("cycling", 5),
-    DRIVING("driving", 1);
+public interface Transportation {
+    String getValue();
+    Integer getCode();
 
-    private final String value;
-    private final Integer code;
+    Transportation PUBLIC_TRANSPORT = Modes.PUBLIC_TRANSPORT;
+    Transportation WALKING_FERRY = Modes.WALKING_FERRY;
+    Transportation CYCLING_FERRY = Modes.CYCLING_FERRY;
+    Transportation DRIVING_FERRY = Modes.DRIVING_FERRY;
+    Transportation WALKING = Modes.WALKING;
+    Transportation CYCLING = Modes.CYCLING;
+    Transportation DRIVING = Modes.DRIVING;
+
+    @Getter
+    @AllArgsConstructor
+    enum Modes implements Transportation {
+        PUBLIC_TRANSPORT("pt", 0),
+        WALKING_FERRY("walking+ferry", 7),
+        CYCLING_FERRY("cycling+ferry", 6),
+        DRIVING_FERRY("driving+ferry", 3),
+        WALKING("walking", 4),
+        CYCLING("cycling", 5),
+        DRIVING("driving", 1);
+
+        private final String value;
+        private final Integer code;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    class Custom implements Transportation {
+        private final String value;
+        private final Integer code;
+    }
 }
