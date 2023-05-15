6378db48fe0a69b0557d21fe2cdb8a0eea9b3a80
danielnaumau <87636735+danielnaumau@users.noreply.github.com>
2023-05-15 17:40:03 +0200

    add new countries (#107)

--- src/main/java/com/traveltime/sdk/dto/requests/proto/Country.java
+++ src/main/java/com/traveltime/sdk/dto/requests/proto/Country.java
@@ -7,13 +7,35 @@ public interface Country {
 
     Country NETHERLANDS = Countries.NETHERLANDS;
     Country AUSTRIA = Countries.AUSTRIA;
+    Country UNITED_KINGDOM = Countries.UNITED_KINGDOM;
     Country BELGIUM = Countries.BELGIUM;
     Country GERMANY = Countries.GERMANY;
     Country FRANCE = Countries.FRANCE;
     Country IRELAND = Countries.IRELAND;
     Country LITHUANIA = Countries.LITHUANIA;
-    Country UNITED_KINGDOM = Countries.UNITED_KINGDOM;
     Country UNITED_STATES = Countries.UNITED_STATES;
+    Country SOUTH_AFRICA = Countries.SOUTH_AFRICA;
+    Country ROMANIA = Countries.ROMANIA;
+    Country PORTUGAL = Countries.PORTUGAL;
+    Country PHILIPPINES = Countries.PHILIPPINES;
+    Country NEW_ZEALAND = Countries.NEW_ZEALAND;
+    Country NORWAY = Countries.NORWAY;
+    Country LATVIA = Countries.LATVIA;
+    Country JAPAN = Countries.JAPAN;
+    Country INDIA = Countries.INDIA;
+    Country INDONESIA = Countries.INDONESIA;
+    Country HUNGARY = Countries.HUNGARY;
+    Country GREECE = Countries.GREECE;
+    Country FINLAND = Countries.FINLAND;
+    Country DENMARK = Countries.DENMARK;
+    Country CANADA = Countries.CANADA;
+    Country AUSTRALIA = Countries.AUSTRALIA;
+    Country SINGAPORE = Countries.SINGAPORE;
+    Country SWITZERLAND = Countries.SWITZERLAND;
+    Country SPAIN = Countries.SPAIN;
+    Country ITALY = Countries.ITALY;
+    Country POLAND = Countries.POLAND;
+    Country SWEDEN = Countries.SWEDEN;
 
     @Value
     class Custom implements Country {
