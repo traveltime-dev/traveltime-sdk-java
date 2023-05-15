cc0b1f2cfcd1639421d54c30915d404b19245ce3
danielnaumau <sadfreddy@gmail.com>
2023-05-15 16:56:54 +0200

    add new countries

--- src/main/java/com/traveltime/sdk/dto/requests/proto/Countries.java
+++ src/main/java/com/traveltime/sdk/dto/requests/proto/Countries.java
@@ -8,13 +8,35 @@ import lombok.Getter;
 public enum Countries implements Country {
     NETHERLANDS("nl"),
     AUSTRIA("at"),
+    UNITED_KINGDOM("uk"),
     BELGIUM("be"),
     GERMANY("de"),
     FRANCE("fr"),
     IRELAND("ie"),
     LITHUANIA("lt"),
-    UNITED_KINGDOM("uk"),
-    UNITED_STATES("us");
+    UNITED_STATES("us"),
+    SOUTH_AFRICA("za"),
+    ROMANIA("ro"),
+    PORTUGAL("pt"),
+    PHILIPPINES("ph"),
+    NEW_ZEALAND("nz"),
+    NORWAY("no"),
+    LATVIA("lv"),
+    JAPAN("jp"),
+    INDIA("in"),
+    INDONESIA("id"),
+    HUNGARY("hu"),
+    GREECE("gr"),
+    FINLAND("fi"),
+    DENMARK("dk"),
+    CANADA("ca"),
+    AUSTRALIA("au"),
+    SINGAPORE("sg"),
+    SWITZERLAND("ch"),
+    SPAIN("es"),
+    ITALY("it"),
+    POLAND("pl"),
+    SWEDEN("se");
 
     private final String value;
 }
