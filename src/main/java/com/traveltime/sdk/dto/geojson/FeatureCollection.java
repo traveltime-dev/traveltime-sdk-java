1c52cb0090ed5fb494fe72c0276f48d028c554ea
daniilnaumov <daniil.naumov@traveltime.com>
2023-08-07 16:13:11 +0200

    add attribution

--- src/main/java/com/traveltime/sdk/dto/geojson/FeatureCollection.java
+++ src/main/java/com/traveltime/sdk/dto/geojson/FeatureCollection.java
@@ -1,5 +1,6 @@
 package com.traveltime.sdk.dto.geojson;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import lombok.experimental.NonFinal;
 import lombok.experimental.SuperBuilder;
@@ -13,6 +14,7 @@ import java.util.List;
 @NonFinal
 @SuperBuilder
 @Jacksonized
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class FeatureCollection<G extends GeoJsonObject, P> {
 
     @NonNull
