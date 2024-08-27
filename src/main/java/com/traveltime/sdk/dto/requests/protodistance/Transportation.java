ea31eff4200c2d19878d92fff0e64eb4f33e52d7
Marcin Januszkiewicz <marcin.januszkiewicz@traveltime.com>
2024-08-27 15:36:17 +0200

    Add cycling transportation mode for proto distance

--- src/main/java/com/traveltime/sdk/dto/requests/protodistance/Transportation.java
+++ src/main/java/com/traveltime/sdk/dto/requests/protodistance/Transportation.java
@@ -9,14 +9,18 @@ public interface Transportation {
 
     Transportation WALKING_FERRY = Transportation.Modes.WALKING_FERRY;
     Transportation DRIVING_FERRY = Transportation.Modes.DRIVING_FERRY;
+    Transportation CYCLING_FERRY = Transportation.Modes.CYCLING_FERRY;
     Transportation WALKING = Transportation.Modes.WALKING;
     Transportation DRIVING = Transportation.Modes.DRIVING;
+    Transportation CYCLING = Transportation.Modes.CYCLING;
 
     @Getter
     @AllArgsConstructor
     enum Modes implements Transportation {
         WALKING_FERRY("walking+ferry", 7),
         DRIVING_FERRY("driving+ferry", 3),
+        CYCLING_FERRY("cycling+ferry", 6),
+        CYCLING("cycling", 5),
         WALKING("walking", 4),
         DRIVING("driving", 1);
 
