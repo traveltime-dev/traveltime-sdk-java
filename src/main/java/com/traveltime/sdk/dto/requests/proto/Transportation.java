03e40fca588ebc281d8a54e8b52c273b86665ee3
daniilnaumov <daniil.naumov@traveltime.com>
2024-09-04 09:14:21 +0200

    rollback Transportation changes

--- src/main/java/com/traveltime/sdk/dto/requests/proto/Transportation.java
+++ src/main/java/com/traveltime/sdk/dto/requests/proto/Transportation.java
@@ -11,6 +11,9 @@ public interface Transportation {
     Transportation WALKING_FERRY = Modes.WALKING_FERRY;
     Transportation CYCLING_FERRY = Modes.CYCLING_FERRY;
     Transportation DRIVING_FERRY = Modes.DRIVING_FERRY;
+    Transportation WALKING = Modes.WALKING;
+    Transportation CYCLING = Modes.CYCLING;
+    Transportation DRIVING = Modes.DRIVING;
 
     @Getter
     @AllArgsConstructor
@@ -18,7 +21,10 @@ public interface Transportation {
         PUBLIC_TRANSPORT("pt", 0),
         WALKING_FERRY("walking+ferry", 7),
         CYCLING_FERRY("cycling+ferry", 6),
-        DRIVING_FERRY("driving+ferry", 3);
+        DRIVING_FERRY("driving+ferry", 3),
+        WALKING("walking", 4),
+        CYCLING("cycling", 5),
+        DRIVING("driving", 1);
 
         private final String value;
         private final Integer code;
