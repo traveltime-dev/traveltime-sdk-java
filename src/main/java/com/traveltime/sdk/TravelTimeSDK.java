52f1b82049924036c713c2dff174d6fb4410b48d
arnasbr <arnas.bradauskas@traveltime.com>
2025-04-11 15:07:25 +0300

    use constants instead of raw status codes

--- src/main/java/com/traveltime/sdk/TravelTimeSDK.java
+++ src/main/java/com/traveltime/sdk/TravelTimeSDK.java
@@ -15,6 +15,7 @@ import okhttp3.*;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Optional;
@@ -80,12 +81,12 @@ public class TravelTimeSDK {
     private <T> Either<TravelTimeError, T> deserializeProtoResponse(ProtoRequest<T> request, Response response) {
         String url = response.request().url().toString();
         int responseCode = response.code();
-        if (responseCode == 404) {
+        if (responseCode == HttpURLConnection.HTTP_NOT_FOUND) {
             RequestError error = new RequestError("Network response is 404 (Not found). Make sure URL " +
                     url + " is correct.");
             return Either.left(error);
         }
-        else if (responseCode == 200) {
+        else if (responseCode == HttpURLConnection.HTTP_OK) {
             Either<TravelTimeError, T> protoResponse = Try
                     .of(() -> Objects.requireNonNull(response.body()).bytes())
                     .toEither()
