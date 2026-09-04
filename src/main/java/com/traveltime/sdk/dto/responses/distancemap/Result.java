97f1d3fdfa2f98f7f89af3217a555f25834e9e4c
arnasbr <arnas.bradauskas@traveltime.com>
2026-09-04 14:10:57 +0300

    Add distance-map endpoint

---
+++ src/main/java/com/traveltime/sdk/dto/responses/distancemap/Result.java
@@ -0,0 +1,16 @@
+package com.traveltime.sdk.dto.responses.distancemap;
+
+import com.traveltime.sdk.dto.common.Shape;
+import java.util.List;
+import lombok.*;
+import lombok.extern.jackson.Jacksonized;
+
+@Value
+@Builder
+@Jacksonized
+@AllArgsConstructor
+public class Result {
+    String searchId;
+    List<Shape> shapes;
+    ResponseProperties properties;
+}
